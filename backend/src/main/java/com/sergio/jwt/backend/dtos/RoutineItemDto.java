5aaec041e37fec049f649edfbeff76d99ed8bd7f
Evan Hammam <evan.hammam@maplarge.com>
2024-07-27 21:07:50 -0400

    Revert "routineitem dto"
    This reverts commit 939cba81eed9f9b6f5c0b628fee5741042b6d93d.

--- backend/src/main/java/com/sergio/jwt/backend/dtos/RoutineItemDto.java
+++ backend/src/main/java/com/sergio/jwt/backend/dtos/RoutineItemDto.java
@@ -1,16 +1,16 @@
 package com.sergio.jwt.backend.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.time.Duration;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoutineItemDto {
-    private String id;
-    private String targetValue;
-    private String amountOfTime; // in milliseconds
+    private Long id;
+    private Long nodeId;
+    private Duration amountOfTime;
+    private double targetValue;
+    private double achievedValue;
 }
